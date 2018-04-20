43a25f43bbb2434762d9b66b07bc26c8ec53487a
Matheus Weber <matheusweber@Matheuss-MacBook-Air.local>
2018-04-19 22:24:04 -0300

    TableView added in view, and delegate methods

--- InvestmentApp/InvestmentApp/Scenes/Form/FormView.swift
+++ InvestmentApp/InvestmentApp/Scenes/Form/FormView.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2018 Matheus Weber. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class FormView: UIView, ViewConfigurationProtocol {
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor.white
+        return tableView
+    }()
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        setupView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setupItems() {
+        self.backgroundColor = UIColor.white
+        
+        addSubview(tableView)
+    }
+    
+    func setupLayout() {
+        tableView.snp.makeConstraints({ (make) in
+            make.top.left.equalTo(16)
+            make.bottom.right.equalTo(-16)
+            make.centerX.equalTo(self)
+        })
+    }
+}
+
--- InvestmentApp/InvestmentApp/Scenes/Form/FormViewController.swift
+++ InvestmentApp/InvestmentApp/Scenes/Form/FormViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
+protocol FormInfoProtocol {
+    func sections() -> [TableSectionable]
+    func itemsInfoSections() -> [TableSectionable]
+}
+
+extension FormInfoProtocol {
+    func sections() -> [TableSectionable] {
+        var sections: [TableSectionable] = []
+        sections.append(contentsOf: itemsInfoSections())
+        return sections
+    }
+}
+
 class FormViewController: UIViewController {
+    var mainView: FormView?
+    var datasource: TableViewSectionableDataSourceDelegate?
+
+    init(mainView: FormView = FormView()) {
+        self.mainView = mainView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        mainView = nil
+        super.init(coder: aDecoder)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDatasource()
+        setupNavigationController()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -19,4 +46,35 @@ class FormViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func loadView() {
+        self.view = mainView
+    }
+    
+    func setupNavigationController() {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    func setupDatasource() {
+        //menuViewModel?.registerCell()
+        datasource = TableViewSectionableDataSourceDelegate(sections: sections())
+        datasource?.delegate = self
+        mainView?.tableView.delegate = datasource
+        mainView?.tableView.dataSource = datasource
+        mainView?.tableView.reloadData()
+    }
+}
+
+extension FormViewController: TableViewSectionableDelegate {
+    func didSelectRowAt(indexPath: IndexPath) {
+        
+    }
+}
+
+
+extension FormViewController: FormInfoProtocol {
+    func itemsInfoSections() -> [TableSectionable] {
+        return []
+    }
+    
 }
