085bbd5a8cb0c8826970f47d7feef372a8a52423
Matheus Weber <matheusweber@Matheuss-MacBook-Air.local>
2018-04-19 22:10:23 -0300

    Error view

--- InvestmentApp/InvestmentApp/Common/Style/UIColor+Ext.swift
+++ InvestmentApp/InvestmentApp/Common/Style/UIColor+Ext.swift
@@ -17,4 +17,8 @@ extension UIColor {
     static var loadingBlack: UIColor {
         return #colorLiteral(red: 0.2666666667, green: 0.2666666667, blue: 0.2666666667, alpha: 1)
     }
+    
+    static var closeGray: UIColor {
+        return #colorLiteral(red: 0.5019607843, green: 0.5058823529, blue: 0.568627451, alpha: 1)
+    }
 }
