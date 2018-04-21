0f19be434d0e21c7f9827f3213e2d68c4876eca5
Gabriel Vieira <viera_frifri@hotmail.com>
2018-04-21 20:58:25 -0300

    lib para loader, Podfile

--- Santander/Santander/ViewController.swift
+++ Santander/Santander/ViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     var test: Bool = true
+    let spinner = NVActivityIndicatorView.init(frame: CGRect(x: 200, y: 200, width: 50, height: 50), type: .circleStrokeSpin, color: .red, padding: 0)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.addSubview(spinner)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,6 +29,9 @@ class ViewController: UIViewController {
     }
     @IBAction func click(_ sender: Any) {
         
+
+        
+        
         var frame = self.label.frame
         
         if self.test {
@@ -31,12 +40,16 @@ class ViewController: UIViewController {
                 self.label.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
                 self.label.frame = frame
             }
+            
+            self.spinner.startAnimating()
         } else {
             frame.origin.y = 300
             UIView.animate(withDuration: 0.1) {
                 self.label.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
                 self.label.frame = frame
             }
+            
+            self.spinner.stopAnimating()
         }
         
         self.test = !self.test
