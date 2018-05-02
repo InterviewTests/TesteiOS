29f7f53655b67dcf4f0a5db96309108f882f2707
Andre Nogueira <avinogueira@yahoo.com>
2018-05-02 00:37:29 -0300

    test santander router

--- SantanderAppTests/SantanderAppTests.swift
+++ SantanderAppTests/SantanderAppTests.swift
@@ -13,23 +13,34 @@ class SantanderAppTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+      
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+       
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSantanderRouter() {
+        let finishedScreen = SantanderRouter.instanceFinishedViewController()
+        XCTAssertNotNil(finishedScreen)
+        
+        let emailTypeField = SantanderRouter.instanceSantanderEmailTypeFieldFromNib()
+        XCTAssertNotNil(emailTypeField)
+        
+        let textField = SantanderRouter.instanceSantanderTextTypeFieldFromNib()
+        XCTAssertNotNil(textField)
+
+        let phoneField = SantanderRouter.instanceSantanderPhoneTypeFieldFromNib()
+        XCTAssertNotNil(phoneField)
+        
+        let fundVC = SantanderRouter.instanceSantanderFundViewController()
+        XCTAssertNotNil(fundVC)
+
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
     
