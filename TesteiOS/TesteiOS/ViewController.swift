dc0f2e28dc1b629b83b156017a8a08a8594a1b6e
Lucas Saito <lucas.eiji.saito@accenture.com>
2018-05-11 14:34:14 -0300

    Adding starter Xcode project

---
+++ TesteiOS/TesteiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesteiOS
+//
+//  Created by lucas.eiji.saito on 10/05/18.
+//  Copyright © 2018 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
