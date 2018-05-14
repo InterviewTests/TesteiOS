ade3b5c8b945273e3475b0a84a434190e50dad28
Luan Henrique Damasceno Costa <luandamasceno@MacBook-Pro-de-Luan.local>
2018-05-14 16:20:14 -0300

    removed pods

--- Pods/Target Support Files/Pods-SantanderExample-SantanderExampleTests/Pods-SantanderExample-SantanderExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SantanderExample_SantanderExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_SantanderExample_SantanderExampleTests
-@end
--- Pods/Target Support Files/Pods-SantanderExample/Pods-SantanderExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SantanderExample : NSObject
-@end
-@implementation PodsDummy_Pods_SantanderExample
-@end
