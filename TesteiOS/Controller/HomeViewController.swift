c99970223d6704162dbdab1833e5162daa7025ce
Naville Brasil <brasil.naville@gmail.com>
2018-05-15 15:51:58 -0300

    Finalizado arquivo xib que ira mostar conteudo de cells.json

---
+++ TesteiOS/Controller/HomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TesteiOS
+//
+//  Created by Naville Brasil on 15/05/18.
+//  Copyright © 2018 Luan Orlando. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController
+{
+    //Outlest
+    @IBOutlet weak var containerInvestment: UIView!
+    @IBOutlet weak var viewBtInvestment: UIView!
+    @IBOutlet weak var viewBtContact: UIView!
+    @IBOutlet weak var buttonInvestment: UIButton!
+    @IBOutlet weak var buttonContact: UIButton!
+    
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
---
+++ TesteiOS/View/Button.swift
@@ -0,0 +1,37 @@
+//
+//  Button.swift
+//  TesteiOS
+//
+//  Created by Naville Brasil on 15/05/18.
+//  Copyright © 2018 Luan Orlando. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class Button: UIButton
+{
+    @IBInspectable
+    var cornerRadius: CGFloat = 0
+    {
+        didSet
+        {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func didMoveToSuperview()
+    {
+        super.didMoveToSuperview()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.backgroundColor = #colorLiteral(red: 0.9215686275, green: 0.462745098, blue: 0.462745098, alpha: 1)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.backgroundColor = #colorLiteral(red: 0.8549019608, green: 0.003921568627, blue: 0.003921568627, alpha: 1)
+    }
+}
