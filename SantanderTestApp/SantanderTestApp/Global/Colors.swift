711a0e10e40d8cf5f534e042c2f3677693f93c8d
theffc <fred_ffc@hotmail.com>
2018-05-19 18:10:09 -0300

    Adding some colors to the project that was on the design

---
+++ SantanderTestApp/SantanderTestApp/Global/Colors.swift
@@ -0,0 +1,47 @@
+//
+//  Colors.swift
+//  SantanderTestApp
+//
+//  Created by Frederico Franco on 19/05/18.
+//  Copyright © 2018 Frederico Franco. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor {
+    
+    class var _grey: UIColor {
+        return #colorLiteral(red: 0.6745098039, green: 0.6745098039, blue: 0.6745098039, alpha: 1)
+    }
+    
+    class var _black: UIColor {
+        return #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    }
+    
+    class var _lightGrey: UIColor {
+        return #colorLiteral(red: 0.937254902, green: 0.9333333333, blue: 0.9294117647, alpha: 1)
+    }
+    
+    class var _red: UIColor {
+        return #colorLiteral(red: 0.8549019608, green: 0.003921568627, blue: 0.003921568627, alpha: 1)
+    }
+    
+    class var _peach: UIColor {
+        return #colorLiteral(red: 0.9215686275, green: 0.462745098, blue: 0.462745098, alpha: 1)
+    }
+    
+    class var _vividRed: UIColor {
+        return #colorLiteral(red: 1, green: 0.1215686275, blue: 0.1215686275, alpha: 1)
+    }
+    
+    class var _green: UIColor {
+        return #colorLiteral(red: 0.3960784314, green: 0.7450980392, blue: 0.1882352941, alpha: 1)
+    }
+    
+    class var _blue: UIColor {
+        return #colorLiteral(red: 0.4274509804, green: 0.631372549, blue: 0.8745098039, alpha: 1)
+    }
+    
+}
+
