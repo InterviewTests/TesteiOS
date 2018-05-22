8377ab3dec51d2fc86fd00a5245b72edbc71cf91
Naville Brasil <brasil.naville@gmail.com>
2018-05-22 11:10:52 -0300

    Finalizado mensagem de sucesso

---
+++ TesteiOS/Controller/SucessViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SucessViewController.swift
+//  TesteiOS
+//
+//  Created by Naville Brasil on 22/05/18.
+//  Copyright © 2018 Luan Orlando. All rights reserved.
+//
+
+import UIKit
+
+class SucessViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendNewMessage(_ sender: UIButton)
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
---
+++ TesteiOS/Model/AcitivityIndicator.swift
@@ -0,0 +1,10 @@
+//
+//  AcitivityIndicator.swift
+//  TesteiOS
+//
+//  Created by Naville Brasil on 22/05/18.
+//  Copyright © 2018 Luan Orlando. All rights reserved.
+//
+
+import Foundation
+
