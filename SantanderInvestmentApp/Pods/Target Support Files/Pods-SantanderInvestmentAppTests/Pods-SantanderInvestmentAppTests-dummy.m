68d92d515894047b3fefca3c7c9a953d56e9ebf2
Michel Sousa <michelsousacarvalho@gmail.com>
2018-05-23 13:26:06 -0300

    Cria projeto, instala pods

---
+++ SantanderInvestmentApp/Pods/Target Support Files/Pods-SantanderInvestmentAppTests/Pods-SantanderInvestmentAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SantanderInvestmentAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_SantanderInvestmentAppTests
+@end
---
+++ SantanderInvestmentApp/SantanderInvestmentApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SantanderInvestmentApp
+//
+//  Created by Michel de Sousa Carvalho on 23/05/18.
+//  Copyright © 2018 Michel de Sousa Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
