4d04bc29defe27913365ff2637f992013069fcff
Eduardo Lombardi <eduardoribeiro@brq.com>
2018-05-29 18:00:12 -0300

    Adicionada ação para a webview dentro da célula

--- TesteiOS/View Controllers/Cells/ThreeLabelTableViewCell.swift
+++ TesteiOS/View Controllers/Cells/ThreeLabelTableViewCell.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import SafariServices
+
+protocol LoadWebView: NSObjectProtocol {
+    func loadNewScreen(controller: UIViewController) -> Void;
+}
 
 class ThreeLabelTableViewCell: UITableViewCell {
+    
+    let urlString = "https://www.google.com.br"
+    weak var delegate: LoadWebView?
     var model:ThreeLabelCellModel = ThreeLabelCellModel(nil,nil,nil,nil)
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +27,18 @@ class ThreeLabelTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+
+    func openWebview() {
+        guard let url = URL(string:self.urlString) else {
+            return
+        }
+        let svc = SFSafariViewController(url:url)
+        if((delegate?.responds(to: Selector(("loadNewScreen:")))) != nil)
+        {
+            delegate?.loadNewScreen(controller:svc);
+        }
     }
 
 }
