57f65cfbbf43c586893a1a7322617097cb47b270
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2018-05-31 18:03:43 -0300

    Correção de clique na tabbar

--- TesteiOS/Utils/CustomTabBarController.swift
+++ TesteiOS/Utils/CustomTabBarController.swift
@@ -11,23 +11,53 @@ import UIKit
 
 class CustomTabBarController: UITabBarController {
     @IBOutlet var customTabBarView: UIView?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        customTabBarView?.frame.size.width = self.view.frame.width
-        customTabBarView?.frame.origin.y = self.view.frame.height - (customTabBarView?.frame.size.height)!
-        self.view.addSubview(customTabBarView!)
-    }
-    @IBAction func changeTab(sender: UIButton) {
-        self.selectedIndex = sender.tag - 1
-        if(sender.tag == 1) {
-            let button = self.view.viewWithTag(2)
-            button?.backgroundColor = UIColor(red: 218/255, green: 1/255, blue: 1/255, alpha: 1.0)
-        } else {
-            let button = self.view.viewWithTag(1)
-            button?.backgroundColor = UIColor(red: 218/255, green: 1/255, blue: 1/255, alpha: 1.0)
-        }
-        sender.backgroundColor = UIColor(red: 191/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    
+    @IBOutlet weak var contactBtn: UIButton?
+    @IBOutlet weak var InvestBtn: UIButton?
+    
+    let selectedColor = UIColor(red: 191/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    let otherColor = UIColor(red: 218/255, green: 1/255, blue: 1/255, alpha: 1.0)
+    
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateLayout()
         
     }
+    
+    
+    func updateLayout() {
+        self.contactBtn?.backgroundColor = selectedColor
+        guard let height = customTabBarView?.frame.size.height else {
+            return
+        }
+        guard let tabView = customTabBarView else {
+            return
+        }
+        tabView.frame.size.width = self.view.frame.width
+        tabView.frame.origin.y = self.view.frame.height - height
+        
+        self.view.addSubview(tabView)
+    }
+    
+    @IBAction func changeTab(sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func investBtnTap(_ sender: Any) {
+        self.selectedIndex = 1
+        self.contactBtn?.backgroundColor = otherColor
+        self.InvestBtn?.backgroundColor = selectedColor
+        
+    }
+    
+    @IBAction func contactBtnTap(_ sender: Any) {
+    self.selectedIndex = 0
+    self.InvestBtn?.backgroundColor =  otherColor
+    self.contactBtn?.backgroundColor = selectedColor
+    }
+    
+    
 }
 
