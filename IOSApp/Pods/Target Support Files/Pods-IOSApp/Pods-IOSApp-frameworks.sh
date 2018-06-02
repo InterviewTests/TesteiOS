effc2b67c46982e066652ae7f798ab26a93b6b87
Unknown <anathomasi@hotmail.com>
2018-06-02 13:41:37 -0300

    Removed unused Pods

--- IOSApp/Pods/Target Support Files/Pods-IOSApp/Pods-IOSApp-frameworks.sh
+++ IOSApp/Pods/Target Support Files/Pods-IOSApp/Pods-IOSApp-frameworks.sh
@@ -141,15 +141,6 @@ strip_invalid_archs() {
   STRIP_BINARY_RETVAL=1
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
-fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
