00150ee91b9aceacf7e5aef6317f3c49fbbb53e7
Unknown <anathomasi@hotmail.com>
2018-06-02 15:33:06 -0300

    Created Utils group to hold Fonts and Extensions

---
+++ IOSApp/IOSApp/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  IOSApp
+//
+//  Created by Ana Beatriz Delavia Thomasi on 02/06/2018.
+//  Copyright © 2018 Ana Beatriz Delavia Thomasi. All rights reserved.
+//
+
+import Foundation
