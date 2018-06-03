6b76605a21d08c557a86435f23d4e54f5911689e
Unknown <anathomasi@hotmail.com>
2018-06-03 11:57:58 -0300

    Created Checkbox component

---
+++ IOSApp/IOSApp/Utils/Checkbox.swift
@@ -0,0 +1,38 @@
+//
+//  Checkbox.swift
+//  IOSApp
+//
+//  Created by Ana Beatriz Delavia Thomasi on 03/06/2018.
+//  Copyright © 2018 Ana Beatriz Delavia Thomasi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckBox: UIButton {
+    // Images
+    let checkedImage = UIImage(named: "checked")! as UIImage
+    let uncheckedImage = UIImage(named: "unchecked")! as UIImage
+    
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: UIControlState.normal)
+            } else {
+                self.setImage(uncheckedImage, for: UIControlState.normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControlEvents.touchUpInside)
+        self.isChecked = false
+    }
+    
+    @objc func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
