7a31978ed7130a92a0aa7c671431dfa44bee1657
Mauricio <mauriciocaserta@gmail.com>
2018-06-08 23:29:44 -0300

    Populando tableView para testes (com os dados obtidos pela API)

--- TesteiOS/Scenes/Form/ListCellViewController.swift
+++ TesteiOS/Scenes/Form/ListCellViewController.swift
@@ -19,12 +19,33 @@ protocol ListCellViewControllerOutput
     func fetchItems()
 }
 
-class ListCellViewController: UIViewController, ListCellViewControllerInput {
+// Tableview
+extension ListCellViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let textFieldCell = tableView.dequeueReusableCell(withIdentifier: "text_field_cell", for: indexPath) as! CustomTextFieldCell
+        
+        return textFieldCell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listCell.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+}
+
+class ListCellViewController: UIViewController, ListCellViewControllerInput, UITableViewDelegate {
     
     var output: ListCellViewControllerOutput!
     var router: ListCellRouter!
     
-    // MARK: - Object lifecycle
+    // Lista de itens
+    var listCell : [Cell] = [Cell]()
+    
+    // UI
+    @IBOutlet weak var tblForm: UITableView!
     
     override func awakeFromNib()
     {
@@ -34,14 +55,26 @@ class ListCellViewController: UIViewController, ListCellViewControllerInput {
     
     override func viewDidLoad(){
         super.viewDidLoad()
+        
+        // Delegate
+        self.tblForm.delegate = self
+        self.tblForm.dataSource = self
+        
+        // Registrar as cells
+        let nibTextFieldCell = UINib(nibName: "CustomTextFieldCell", bundle: nil)
+        self.tblForm.register(nibTextFieldCell, forCellReuseIdentifier: "text_field_cell")
+        
         output.fetchItems()
     }
     
+    // Métodos de callback das requisições
     func successFetchedItems(response: ListCell.Fetch.Response) {
-        print(response.cells)
+        listCell = response.cells
+        // Recarregar os dados da tabela
+        self.tblForm.reloadData()
     }
     
     func errorFetchingItems(response: ListCell.Fetch.Response) {
-        
+        //
     }
 }
