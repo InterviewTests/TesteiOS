8e0fdb13012e848169d310b654d5279a0716da89
carolbonturi <carolbonturi@gmail.com>
2018-07-01 20:10:22 -0300

    Format phone number input.

--- TesteiOS/TesteiOS/ContactViewController.swift
+++ TesteiOS/TesteiOS/ContactViewController.swift
@@ -60,6 +60,57 @@ class ContactViewController: UIViewController, UITextFieldDelegate {
         
     }
     
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        var fullString = textField.text ?? ""
+        fullString.append(string)
+        
+        if range.length == 1 {
+            textField.text = format(phoneNumber: fullString, shouldRemoveLastDigit: true)
+        } else {
+            textField.text = format(phoneNumber: fullString)
+        }
+        return false
+    }
+    
+    
+    func format(phoneNumber: String, shouldRemoveLastDigit: Bool = false) -> String {
+        guard !phoneNumber.isEmpty else { return "" }
+        guard let regex = try? NSRegularExpression(pattern: "[\\s-\\(\\)]", options: .caseInsensitive) else { return "" }
+        let r = NSString(string: phoneNumber).range(of: phoneNumber)
+        var number = regex.stringByReplacingMatches(in: phoneNumber, options: .init(rawValue: 0), range: r, withTemplate: "")
+        
+        // max 11 numbers
+        if number.count > 11 {
+            let eleventhDigitIndex = number.index(number.startIndex, offsetBy: 11)
+            number = String(number[number.startIndex..<eleventhDigitIndex])
+        }
+        
+        if shouldRemoveLastDigit {
+            let end = number.index(number.startIndex, offsetBy: number.count-1)
+            number = String(number[number.startIndex..<end])
+        }
+        
+        if number.count < 7 {
+            let end = number.index(number.startIndex, offsetBy: number.count)
+            let range = number.startIndex..<end
+            number = number.replacingOccurrences(of: "(\\d{2})(\\d+)", with: "($1) $2", options: .regularExpression, range: range)
+            
+        } else if number.count < 11 {
+                let end = number.index(number.startIndex, offsetBy: number.count)
+                let range = number.startIndex..<end
+                number = number.replacingOccurrences(of: "(\\d{2})(\\d{4})(\\d+)", with: "($1) $2-$3", options: .regularExpression, range: range)
+        } else {
+            let end = number.index(number.startIndex, offsetBy: number.count)
+            let range = number.startIndex..<end
+            number = number.replacingOccurrences(of: "(\\d{2})(\\d{5})(\\d+)", with: "($1) $2-$3", options: .regularExpression, range: range)
+        }
+        
+        return number
+    }
+    
+    
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print(textField.tag)
         // editing name
