f4558eb80e496fa2263b2301ba0299bb7cbc7a85
stborges <feliborgez@gmail.com>
2018-07-08 17:27:06 -0300

    Add UIColor extensions
    Extensions are:
        - Hex: initializer for hexa values
        - CustomColors: struct for defining static custom colors

---
+++ TesteiOS/TesteiOS/Utils/UIColor+CustomColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+CustomColor.swift
+//  TesteiOS
+//
+//  Created by Felipe Borges on 08/07/18.
+//  Copyright © 2018 Felipe Borges. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    struct CustomColor {
+        static let vividRed = UIColor(hex: 0xDA0101)
+        static let strongRed = UIColor(hex: 0xC80404)
+    }
+}
---
+++ TesteiOS/TesteiOS/Utils/UIColor+Hex.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Hex.swift
+//  TesteiOS
+//
+//  Created by Felipe Borges on 08/07/18.
+//  Copyright © 2018 Felipe Borges. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(hex:Int) {
+        self.init(red:(hex >> 16) & 0xff, green:(hex >> 8) & 0xff, blue:hex & 0xff)
+    }
+}
