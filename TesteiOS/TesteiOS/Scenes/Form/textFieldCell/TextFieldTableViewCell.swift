182f71d030b2bee23acbf42315f2de752bf4a345
Lucas Saito <lucas.eiji.saito@accenture.com>
2018-07-10 20:51:19 -0300

    Add validation to Form fields

--- TesteiOS/TesteiOS/Scenes/Form/textFieldCell/TextFieldTableViewCell.swift
+++ TesteiOS/TesteiOS/Scenes/Form/textFieldCell/TextFieldTableViewCell.swift
@@ -17,6 +17,43 @@ class TextFieldTableViewCell: UITableViewCell {
     @IBAction func buttonClearTouchUpInside(_ sender: UIButton) {
         textFieldInput.text = ""
     }
+    @IBAction func textFieldDidBegin(_ sender: UITextField) {
+        if ((sender.text ?? "").count == 0) {
+            viewBottomLine.backgroundColor = UIColor.lightGray
+        }
+    }
+    @IBAction func textFieldDidEnd(_ sender: UITextField) {
+        if ((sender.text ?? "").count == 0) {
+            viewBottomLine.backgroundColor = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
+        }
+    }
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        let colorValid = UIColor(red: 155.0/255.0, green: 206.0/255.0, blue: 111.0/255.0, alpha: 1.0)
+        let colorInvalid = UIColor(red: 239.0/255.0, green: 103.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+        if (viewModel.type == .field) {
+            if (viewModel.typeField! == .text) {
+                if ((sender.text ?? "").count > 0) {
+                    viewBottomLine.backgroundColor = colorValid
+                } else {
+                    viewBottomLine.backgroundColor = colorInvalid
+                }
+            }
+            if (viewModel.typeField! == .email) {
+                if (isValidEmail(testStr: sender.text ?? "")) {
+                    viewBottomLine.backgroundColor = colorValid
+                } else {
+                    viewBottomLine.backgroundColor = colorInvalid
+                }
+            }
+            if (viewModel.typeField! == .telNumber) {
+                if (isValidTelNumber(testStr: sender.text ?? "")) {
+                    viewBottomLine.backgroundColor = colorValid
+                } else {
+                    viewBottomLine.backgroundColor = colorInvalid
+                }
+            }
+        }
+    }
     
     var viewModel: Form.FormCell.ViewModel = Form.FormCell.ViewModel() {
         didSet {
@@ -48,5 +85,19 @@ class TextFieldTableViewCell: UITableViewCell {
         self.contentView.layoutIfNeeded()
         
         labelTextField.text = viewModel.message
+    }
+    
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
+    func isValidTelNumber(testStr:String) -> Bool {
+        let telNumberRegEx = "^\\([1-9]{2}\\) [2-9][0-9]{3,4}\\-[0-9]{4}$"
+        
+        let telNumberTest = NSPredicate(format:"SELF MATCHES %@", telNumberRegEx)
+        return telNumberTest.evaluate(with: testStr)
     }
 }
