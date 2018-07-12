54ebf23464f1c733b34e0fd8ebbb432097808171
Andre Nogueira <avinogueira@yahoo.com>
2018-07-11 22:41:29 -0300

    refactor

--- SantanderApp/AppDelegate.swift
+++ SantanderApp/AppDelegate.swift
@@ -16,11 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let storyboard = UIStoryboard(name: "santanderapp", bundle: nil)
-        let initialViewController = storyboard.instantiateViewController(withIdentifier: "SantanderCustomerViewController")
-        self.window?.rootViewController = initialViewController
-        self.window?.makeKeyAndVisible()
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        let storyboard = UIStoryboard(name: "santanderapp", bundle: nil)
+//        let initialViewController = storyboard.instantiateViewController(withIdentifier: "SantanderCustomerViewController")
+//        self.window?.rootViewController = initialViewController
+//        self.window?.makeKeyAndVisible()
         
         return true
     }
