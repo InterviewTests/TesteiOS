f32179c805cab912fad742448cbc65939c91449e
Gustavo <gustavo.sore@pixit.com.br>
2018-07-20 11:57:10 -0300

    Add Connection feature.
    For check internet access

---
+++ FinForm/Utils/Connection.swift
@@ -0,0 +1,38 @@
+//
+//  Connection.swift
+//  FinForm
+//
+//  Created by Gustavo on 20/07/18.
+//  Copyright © 2018 Sore. All rights reserved.
+//
+
+import SystemConfiguration
+
+class Connection{
+    
+    static func isInternetAvailable() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+    
+}
