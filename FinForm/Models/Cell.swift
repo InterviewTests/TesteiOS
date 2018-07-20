cc5084582fec88b9c9760c44b8d0058c8355ce93
Gustavo <gustavo.sore@pixit.com.br>
2018-07-20 11:18:33 -0300

    Add Cell Model

---
+++ FinForm/Models/Cell.swift
@@ -0,0 +1,34 @@
+//
+//  Cell.swift
+//  FinForm
+//
+//  Created by Gustavo on 20/07/18.
+//  Copyright © 2018 Sore. All rights reserved.
+//
+
+import Foundation
+
+struct Cell: Codable
+{
+    var id: Int?
+    var type: Type?
+    var message: String?
+    var typeField: TypeField?
+    var topSpacing: Float?
+    var show: Int?
+    var required: Bool?
+}
+
+enum Type: Int,Codable{
+    case field = 1
+    case text = 2
+    case image = 3
+    case checkbox = 4
+    case send = 5
+}
+
+enum TypeField: Int,Codable{
+    case text = 1
+    case telNumber = 2
+    case email = 3
+}
