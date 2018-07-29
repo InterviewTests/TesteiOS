58696f6ff62d0fb6eba4b0c293b25ad81b91b5ef
Erick Peripolli <erick.peripolli@accenture.com>
2018-07-29 12:29:16 -0300

    Created Xcode project

---
+++ iOSTest/iOSTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by erick.peripolli on 29/07/18.
+//  Copyright © 2018 Erick Peripolli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
