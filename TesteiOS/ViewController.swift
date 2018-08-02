6ff04ebae065fa51868b388bbad3cd6d710987d8
Dafle <dafle@outlook.com>
2018-08-02 11:00:01 -0300

    Adicionando icones e Fontes

---
+++ TesteiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesteiOS
+//
+//  Created by Dafle Cardoso on 02/08/2018.
+//  Copyright © 2018 Dafle Cardoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
