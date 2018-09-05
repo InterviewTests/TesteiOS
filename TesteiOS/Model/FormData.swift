a1ebeeae5b5ee02a4fe3733f5cbb229bdf3f35f9
Andre Paganin <andrepaganin@icloud.com>
2018-09-05 12:38:11 -0300

    API request Utilizando o Alamofire.

---
+++ TesteiOS/Model/FormData.swift
@@ -0,0 +1,63 @@
+//
+//  FormData.swift
+//  TesteiOS
+//
+//  Created by Andre Paganin on 04/09/18.
+//  Copyright © 2018 Andre Paganin. All rights reserved.
+//
+
+import Foundation
+
+
+struct FormData: Decodable {
+    let cells: [FormCell]?
+}
+
+struct FormCell {
+    let id: Int?
+    let type: Int?
+    let message: String?
+    let typefield: String?
+    let hidden: Bool?
+    let show: Int?
+    let topSpacing: Double?
+    let required: Bool?
+}
+
+extension FormCell: Decodable {
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case type = "type"
+        case message = "message"
+        case typefield = "typefield"
+        case hidden = "hidden"
+        case show = "show"
+        case topSpacing = "topSpacing"
+        case required = "required"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let id = try container.decode(Int?.self, forKey: .id)
+        let type = try container.decode(Int?.self, forKey: .type)
+        let message = try container.decode(String?.self, forKey: .message)
+        let hidden = try container.decode(Bool?.self, forKey: .hidden)
+        let show = try container.decode(Int?.self, forKey: .show)
+        let topSpacing = try container.decode(Double?.self, forKey: .topSpacing)
+        let required = try container.decode(Bool?.self, forKey: .required)
+
+        var typefieldStr: String?
+        do {
+            typefieldStr = try container.decode(String?.self, forKey: .typefield)
+        } catch {
+            if let typefieldInd = try container.decode(Int?.self, forKey: .typefield) {
+                typefieldStr = String(typefieldInd)
+            }
+        }
+        self.init(id: id, type: type, message: message, typefield: typefieldStr, hidden: hidden, show: show, topSpacing: topSpacing, required: required)
+    }
+}
+
+
