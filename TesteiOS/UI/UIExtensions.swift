f9261c5c95f05d0e61011d4042b59976b48f32c3
Andre Paganin <andrepaganin@icloud.com>
2018-09-05 12:44:42 -0300

    UI > Add fontes do projetos ( DINPro e Font Awesome 5 Free.

---
+++ TesteiOS/UI/UIExtensions.swift
@@ -0,0 +1,46 @@
+//
+//  UIExtensions.swift
+//  TesteiOS
+//
+//  Created by Andre Paganin on 04/09/18.
+//  Copyright © 2018 Andre Paganin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+
+    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
+
+extension UIFont {
+
+    public class func fontDIN(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont(name: "DINPro", size: fontSize)!
+    }
+    
+    public class func fontAwesome(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont(name: "Font Awesome 5 Free", size: fontSize)!
+    }
+}
+
+extension UIColor {
+    
+    public class func appPrimaryColor() -> UIColor {
+        return UIColor(red: 204/255, green: 40/255, blue: 15/255, alpha: 1.0)
+    }
+}
+
+
+
