dcaf3f01d9ff3cfd51ee13fba4022aa94dec0b46
Adolfho Athyla <adolfo.athyla@gmail.com>
2018-09-07 16:57:46 -0300

    > Estruturando arquitetura do projeto. > Definindo modelagem de células. > Realizando requisição dos metadados do formulário.

---
+++ Investimentos/Investimentos/Models/Form/Cell.swift
@@ -0,0 +1,21 @@
+//
+//  Cell.swift
+//  Investimentos
+//
+//  Created by Adolfho Athyla on 07/09/2018.
+//  Copyright © 2018 a7hyla. All rights reserved.
+//
+
+import UIKit
+import EVReflection
+
+class Cell: EVObject {
+    var id: NSNumber?
+    var type: NSNumber?
+    var message: String?
+    var typefield: NSNumber?
+    var hidden = false
+    var topSpacing: NSNumber?
+    var show: NSNumber?
+    var required = false
+}
