9f0c113251fdd6771264a1408f949404bf9a5447
Adolfho Athyla <adolfo.athyla@gmail.com>
2018-09-07 14:37:20 -0300

    > Estrutura inicial do projeto. > Adicionando pods Alamofire e EVReflection, que irá nos ajudar a realizar as requisições e mapear os objetos do json, respectivamente.

---
+++ Investimentos/Investimentos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Investimentos
+//
+//  Created by Adolfho Athyla on 07/09/2018.
+//  Copyright © 2018 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
