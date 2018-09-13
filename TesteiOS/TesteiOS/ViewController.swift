b376aa094726cd1834a7994c2493f22f877a288d
Lucas Fernandez Nicolau <dev.lucasfn@gmail.com>
2018-09-13 13:22:31 -0300

    Criação do projeto no Xcode

---
+++ TesteiOS/TesteiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesteiOS
+//
+//  Created by Nicolau on 13/09/18.
+//  Copyright © 2018 {lfn}. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
