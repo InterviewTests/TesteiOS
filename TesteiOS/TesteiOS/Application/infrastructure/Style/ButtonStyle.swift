a1624d61e127d5d5674e91827af2f33d4cccc2d9
Anderson Silva <anderson.jesus.silva@accenture.com>
2019-05-07 14:08:08 -0300

    Add classes de estilo para Cores e Botões.

---
+++ TesteiOS/TesteiOS/Application/infrastructure/Style/ButtonStyle.swift
@@ -0,0 +1,64 @@
+//
+//  ButtonStyle.swift
+//  TesteiOS
+//
+//  Created by anderson.jesus.silva on 07/05/19.
+//  Copyright © 2019 anderson.jesus.silva. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class ButtonStyle : UIButton {
+    
+    let gradientLayer = CAGradientLayer()
+    
+    @IBInspectable var cornerRadius : CGFloat = 0{
+        didSet{
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth : CGFloat = 0 {
+        didSet{
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColorCV : UIColor? {
+        didSet {
+            layer.borderColor = borderColorCV?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var topGradientColor: UIColor? {
+        didSet {
+            setGradient(topGradientColor: topGradientColor, bottomGradientColor: bottomGradientColor)
+        }
+    }
+    
+    @IBInspectable
+    var bottomGradientColor: UIColor? {
+        didSet {
+            setGradient(topGradientColor: topGradientColor, bottomGradientColor: bottomGradientColor)
+        }
+    }
+    
+    private func setGradient(topGradientColor: UIColor?, bottomGradientColor: UIColor?) {
+        if let topGradientColor = topGradientColor, let bottomGradientColor = bottomGradientColor {
+            gradientLayer.frame = bounds
+            gradientLayer.colors = [topGradientColor.cgColor, bottomGradientColor.cgColor]
+            gradientLayer.borderColor = layer.borderColor
+            gradientLayer.borderWidth = layer.borderWidth
+            gradientLayer.cornerRadius = layer.cornerRadius
+            layer.insertSublayer(gradientLayer, at: 0)
+        } else {
+            gradientLayer.removeFromSuperlayer()
+        }
+    }
+    
+}
+
+
---
+++ TesteiOS/TesteiOS/Application/infrastructure/extensions/Extension+UIColor.swift
@@ -0,0 +1,61 @@
+//
+//  Extension+UIColor.swift
+//  TesteiOS
+//
+//  Created by anderson.jesus.silva on 07/05/19.
+//  Copyright © 2019 anderson.jesus.silva. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static func btnSend() -> UIColor {
+        return UIColor(red: 218/255.0, green: 1/255.0, blue: 1/255.0, alpha: 1.0)
+    }
+    
+    static func txLabel51() -> UIColor {
+        return UIColor(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1.0)
+    }
+    
+    static func txLabel172() -> UIColor {
+        return UIColor(red: 172/255.0, green: 172/255.0, blue: 172/255.0, alpha: 1.0)
+    }
+    
+    static func borderColor() -> UIColor {
+        return UIColor(red: 151/255.0, green: 151/255.0, blue: 151/255.0, alpha: 1.0)
+    }
+    
+    static func labelWhite() -> UIColor {
+        return UIColor.white
+    }
+    
+    static func textFieldFailure() -> UIColor {
+        return UIColor(red: 255/255.0, green: 31/255.0, blue: 31/255.0, alpha: 1.0)
+    }
+    
+    static func textFieldSuccess() -> UIColor {
+        return UIColor(red: 101/255.0, green: 190/255.0, blue: 48/255.0, alpha: 1.0)
+    }
+    
+    static func swColorOne() -> UIColor {
+        return UIColor(red: 116/255.0, green: 218/255.0, blue: 97/255.0, alpha: 1.0)
+    }
+    
+    static func swColorTwo() -> UIColor {
+        return UIColor(red: 74/255.0, green: 193/255.0, blue: 108/255.0, alpha: 1.0)
+    }
+    
+    static func swColorTree() -> UIColor {
+        return UIColor(red: 255/255.0, green: 192/255.0, blue: 17/255.0, alpha: 1.0)
+    }
+    
+    static func swColorFour() -> UIColor {
+        return UIColor(red: 255/255.0, green: 116/255.0, blue: 44/255.0, alpha: 1.0)
+    }
+    
+    static func swColorFive() -> UIColor {
+        return UIColor(red: 255/255.0, green: 54/255.0, blue: 52/255.0, alpha: 1.0)
+    }
+}
