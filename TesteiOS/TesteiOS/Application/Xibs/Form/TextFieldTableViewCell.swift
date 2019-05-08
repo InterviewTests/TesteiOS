eb17a1ab9ba601fafcbfe3be299188edd265bcae
Anderson Silva <anderson.jesus.silva@accenture.com>
2019-05-08 15:43:10 -0300

    Validações nos campos.

--- TesteiOS/TesteiOS/Application/Xibs/Form/TextFieldTableViewCell.swift
+++ TesteiOS/TesteiOS/Application/Xibs/Form/TextFieldTableViewCell.swift
@@ -9,16 +9,14 @@
 import UIKit
 import MaterialComponents
 
-protocol TextFieldTableViewCellDelegate: class {
-    func validate(field:MDCTextField)
-}
-
 class TextFieldTableViewCell: UITableViewCell {
 
     @IBOutlet weak var txField: MDCTextField!
     
-    var txFieldController: MDCTextInputControllerLegacyDefault?
-    weak var delgate:TextFieldTableViewCellDelegate?
+    private var txFieldController: MDCTextInputControllerLegacyDefault?
+    private var type:Type!
+    private var typeField:TypeField!
+    private var validator:Validator!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -41,16 +39,17 @@ class TextFieldTableViewCell: UITableViewCell {
             return
         }
         
-        let textFieldType:TypeField = TypeField.fromString(typeTF as! String)
-        let cellType:Type = Type(rawValue: typeCell)!
+
+        self.typeField = TypeField.fromString("\(typeTF)")
+        self.type = Type(rawValue: typeCell)!
+        self.validator = Validator(typefield: self.typeField)
         
         self.txFieldController = MDCTextInputControllerLegacyDefault(textInput: self.txField)
         
-        self.txFieldController?.placeholderText = textFieldType.titleField
+        self.txFieldController?.floatingPlaceholderActiveColor = UIColor.borderColor()
+        self.txFieldController?.placeholderText = self.typeField.titleField
         
-        self.txFieldController?.disabledColor = UIColor.borderColor()
-
-        self.delgate?.validate(field: self.txField)
+        self.txField.delegate = self
         
     }
     
@@ -59,4 +58,89 @@ class TextFieldTableViewCell: UITableViewCell {
         tableView.register(nib, forCellReuseIdentifier: "TextFieldTableViewCell")
     }
     
+}
+
+extension TextFieldTableViewCell: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch self.validator.typefield {
+        case .email:
+            let text = textField.text ?? ""
+            if self.validator.validateEmail(email: text) {
+                self.txFieldController?.activeColor = UIColor.textFieldSuccess()
+            }else{
+                self.txFieldController?.activeColor = UIColor.textFieldFailure()
+            }
+        case .telNumber:
+            let text = textField.text ?? ""
+            if self.validator.validatePhone(phone: text) {
+                self.txFieldController?.activeColor = UIColor.textFieldSuccess()
+            }else{
+                self.txFieldController?.activeColor = UIColor.textFieldFailure()
+            }
+        default:
+            let text = textField.text ?? ""
+            if self.validator.validate(text) {
+                self.txFieldController?.activeColor = UIColor.textFieldSuccess()
+            }else{
+                self.txFieldController?.activeColor = UIColor.textFieldFailure()
+            }
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        switch self.validator.typefield {
+        case .email:
+            if let text = textField.text {
+                if let textRange = Range(range, in: text) {
+                    let updateText = text.replacingCharacters(in: textRange, with: string)
+                    
+                    if updateText.isEmpty {
+                        self.txFieldController?.activeColor = UIColor.borderColor()
+                    }else{
+                        if self.validator.validateEmail(email: updateText) {
+                            self.txFieldController?.activeColor = UIColor.textFieldSuccess()
+                        }else{
+                            self.txFieldController?.activeColor = UIColor.textFieldFailure()
+                        }
+                    }
+                    
+                }
+                
+            }
+        case .telNumber:
+            var text = textField.text ?? ""
+            text.append(string)
+            if range.length == 1 {
+                self.txFieldController?.activeColor = UIColor.textFieldFailure()
+                textField.text = self.validator.format(phoneNumber: text, shouldRemoveLastDigit: true)
+            } else {
+                textField.text = self.validator.format(phoneNumber: text)
+                if self.validator.validatePhone(phone: textField.text!) {
+                    self.txFieldController?.activeColor = UIColor.textFieldSuccess()
+                }else{
+                    self.txFieldController?.activeColor = UIColor.textFieldFailure()
+                }
+            }
+            return false
+        default:
+            if let text = textField.text {
+                if let textRange = Range(range, in: text) {
+                    let updateText = text.replacingCharacters(in: textRange, with: string)
+                    
+                    if updateText.isEmpty {
+                        self.txFieldController?.activeColor = UIColor.borderColor()
+                    }else{
+                        if self.validator.validate(updateText) {
+                            self.txFieldController?.activeColor = UIColor.textFieldSuccess()
+                        }else{
+                            self.txFieldController?.activeColor = UIColor.textFieldFailure()
+                        }
+                    }
+                    
+                }
+            }
+        }
+        return true
+    }
 }
