f8721bfe59a6dd7c20b2be200d805c9183752f3c
Matheus Ribeiro <matheusr30@icloud.com>
2019-05-16 03:33:12 -0400

    Implementação da TabBarController

---
+++ Santander-Test/Santander-Test/Scenes/MainTabBar/MainTabBar.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBar.swift
+//  Santander-Test
+//
+//  Created by Matheus Ribeiro on 16/05/19.
+//  Copyright © 2019 Matheus Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBar: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = UIColor.darkRedColor
+    }
+    
+}
