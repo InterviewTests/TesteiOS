44bbcbe829e70ed575ff720c2b69789907210e6d
Luiz Felipe <luizfelipealbernaz@hotmail.com>
2019-07-06 13:18:06 -0300

    Created extension for TableView

---
+++ SantanderChallenge/SantanderChallenge/Extensions/UITableView+Extension.swift
@@ -0,0 +1,24 @@
+//
+// UITableView+Extension.swift
+// SantanderChallenge
+//
+// Created for SantanderChallenge on 06/07/19.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func registerCellNib(cellClass: UITableViewCell.Type) {
+        let name = cellClass.reusableIdentifier
+        let nib = UINib(nibName: name, bundle: nil)
+        self.register(nib, forCellReuseIdentifier: name)
+    }
+    
+    func registerCellsNib(cellsClass: [UITableViewCell.Type]) {
+        cellsClass.forEach {
+            registerCellNib(cellClass: $0)
+        }
+    }
+}
+
