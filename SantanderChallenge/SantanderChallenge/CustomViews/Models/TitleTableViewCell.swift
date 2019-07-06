14e8c1e9a36b42c36e8f835febb9038ad18387e1
Luiz Felipe <luizfelipealbernaz@hotmail.com>
2019-07-06 12:23:16 -0300

    Added fonts

---
+++ SantanderChallenge/SantanderChallenge/CustomViews/Models/TitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+// TitleTableViewCell.swift
+// SantanderChallenge
+//
+// Created for SantanderChallenge on 06/07/19.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
