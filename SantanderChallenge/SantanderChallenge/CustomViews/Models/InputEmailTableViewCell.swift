5ee843f8f8b9c4c7f638e947ac5b5021c20dd078
Luiz Felipe <luizfelipealbernaz@hotmail.com>
2019-07-06 12:43:08 -0300

    Created cells for Email and Phone input

---
+++ SantanderChallenge/SantanderChallenge/CustomViews/Models/InputEmailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+// InputEmailTableViewCell.swift
+// SantanderChallenge
+//
+// Created for SantanderChallenge on 06/07/19.
+//
+
+import UIKit
+
+class InputEmailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SantanderChallenge/SantanderChallenge/CustomViews/Models/InputPhoneTableViewCell/InputPhoneTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+// InputPhoneTableViewCell.swift
+// SantanderChallenge
+//
+// Created for SantanderChallenge on 06/07/19.
+//
+
+import UIKit
+
+class InputPhoneTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
