6ce14cd4b44a2f921a9f28db66f5e5d3b00177a4
Luiz Felipe <luizfelipealbernaz@hotmail.com>
2019-07-05 22:11:25 -0300

    Created network provider to make requests

--- SantanderChallenge/SantanderChallenge/Services/Providers/ProductionNetworkProvider.swift
+++ SantanderChallenge/SantanderChallenge/Services/Providers/ProductionNetworkProvider.swift
@@ -7,27 +7,55 @@
 
 import Foundation
 
+fileprivate enum Path: String {
+    case register = "https://floating-mountain-50292.herokuapp.com/cells.json"
+}
+
 class ProductionNetworkProvider: NetworkProviderProtocol {
-    func fetchFormData(_ completion: @escaping (Bool) -> Void) {
-        
+    
+    func fetchFormData(_ completion: @escaping (NetworkResponse) -> Void) {
+        request(at: Path.register.rawValue) { (result) in
+            completion(result)
+        }
+    }
+    
+    func fetchFundsFields(_ completion: @escaping (NetworkResponse) -> Void) {
+    }
+}
+
+extension ProductionNetworkProvider {
+    private func request(at path: String, _ completion: @escaping (Result<Data, NetworkError>) -> Void) {
         let configuration = URLSessionConfiguration.default
         let session = URLSession(configuration: configuration)
         
-        let task = session.dataTask(with: URL(string: "https://floating-mountain-50292.herokuapp.com/cells.json")!) { (data, response, error) in
-            completion(true)
-            print("Data: ", data ?? "")
-            print("Response: ", response ?? "")
-            print("Error: ", error ?? "")
+        guard let url = URL(string: path) else {
+            completion(.failure(NetworkError.invalidPath))
+            return
         }
         
+        let task = session.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error {
+                completion(.failure(NetworkError.responseError(error.localizedDescription)))
+                return
+            }
+            
+            guard self.statusCode(fromResponse: response) == 200 else {
+                completion(.failure(NetworkError.invalidStatusCode))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.missingResponseData))
+                return
+            }
+            
+            completion(.success(data))
+        }
         task.resume()
     }
     
-    func fetchFundsFields(_ completion: @escaping (Bool) -> Void) {
-        
+    private func statusCode(fromResponse response: URLResponse?) -> Int? {
+        return (response as? HTTPURLResponse)?.statusCode
     }
-    
-
-    
-    
 }
