3c38104944d7aeaa8641a681739d59f8617a0f2f
Orlando Amorim <orlando.filho95@icloud.com>
2019-08-11 02:11:53 -0300

    Add base project

---
+++ Santander/Santander/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Santander
+//
+//  Created by Orlando Amorim on 10/08/19.
+//  Copyright © 2019 Santander. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ContainerViewController()
+        return true
+    }
+}
+
---
+++ Santander/Santander/Scenes/Container/ContainerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ContainerViewController.swift
+//  Santander
+//
+//  Created by Orlando Amorim on 10/08/19.
+//  Copyright © 2019 Santander. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ContainerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+}
