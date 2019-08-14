b30f0cc5257082bdad7f48f7105274cdbc8a4fcd
Orlando Amorim <orlando.filho95@icloud.com>
2019-08-14 00:42:52 -0300

    Add ContainerViewController

---
+++ Santander/Santander/Scenes/Container/ContainerViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ContainerViewController.swift
+//  Santander
+//
+//  Created by Orlando Amorim on 10/08/19.
+//  Copyright © 2019 Santander. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ContainerViewController: UIViewController {
+    
+    //MARK: - Views
+    private var containedSegmentedView: SegmentedControl = {
+        var segmentedControl = SegmentedControl()
+        segmentedControl.set(buttons: [("Investimento", true), ("Contato", false)])
+        return segmentedControl
+    }()
+    
+    private lazy var fundsViewController: ContactViewController = {
+        let viewController = ContactViewController()
+        return viewController
+    }()
+    
+    private lazy var contactViewController: ContactViewController = {
+        let viewController = ContactViewController()
+        return viewController
+    }()
+
+    enum ContainerType: Int {
+        case funds = 0
+        case contact = 1
+        
+        var title: String {
+            switch self {
+            case .funds:
+                return "Investimento"
+            case .contact:
+                return "Contato"
+            }
+        }
+    }
+    
+    //MARK: - Vars
+    private var selectedType: ContainerType = .funds
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupSelectionHandler()
+        select(type: .funds)
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        addContainedSegmentedView()
+        setupNavigationController()
+    }
+    
+    private func setupNavigationController() {
+        guard let navigationController = navigationController else {
+            return
+        }
+        navigationController.navigationBar.titleTextAttributes =
+            [.foregroundColor: UIColor.red,
+             .font: UIFont.santander(type: .medium, with: 16.0)]
+        if #available(iOS 11.0, *) {
+            navigationController.navigationBar.prefersLargeTitles = false
+        }
+        navigationController.navigationBar.backgroundColor = .black
+        navigationController.navigationBar.shadowImage = UIImage()
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.view.backgroundColor = .white
+        navigationController.navigationBar.tintColor = .white
+    }
+    
+    private func addContainedSegmentedView() {
+        view.addSubview(containedSegmentedView)
+        containedSegmentedView.snp.makeConstraints { make in
+            make.height.equalTo(57.0)
+            make.leading.trailing.equalToSuperview()
+            if #available(iOS 11.0, *) {
+                make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+            } else {
+                make.bottom.equalToSuperview()
+            }
+        }
+    }
+    
+    private func setupSelectionHandler() {
+        containedSegmentedView.onTap { [weak self] index in
+            guard let self = self, let type = ContainerType(rawValue: index) else {
+                return
+            }
+            self.select(type: type)
+        }
+    }
+    
+    private func select(type: ContainerType) {
+        let viewControllers = [fundsViewController, contactViewController]
+        removeChildVc(viewControllers[type.rawValue])
+        addChildVc(viewControllers[type.rawValue])
+        title = type.title
+        selectedType = type
+    }
+    
+    private func addChildVc(_ child: UIViewController) {
+        addChild(child)
+        view.insertSubview(child.view, belowSubview: containedSegmentedView)
+        child.view.snp.makeConstraints { (make) in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(containedSegmentedView.snp.top)
+        }
+        child.didMove(toParent: self)
+    }
+
+    private func removeChildVc(_ child: UIViewController) {
+        child.willMove(toParent: self)
+        child.removeFromParent()
+        child.view.removeFromSuperview()
+    }
+}
