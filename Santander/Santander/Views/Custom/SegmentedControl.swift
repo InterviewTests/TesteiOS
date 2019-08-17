f888f7359fb86e4c8b75f9a4ffa901261dda0ab3
Orlando Amorim <orlando.filho95@icloud.com>
2019-08-17 18:21:00 -0300

    Add handler to device rotation

--- Santander/Santander/Views/Custom/SegmentedControl.swift
+++ Santander/Santander/Views/Custom/SegmentedControl.swift
@@ -41,8 +41,8 @@ class SegmentedControl: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    override func layoutSubviews() {
+        super.layoutSubviews()
         updateSelectorView()
     }
     
