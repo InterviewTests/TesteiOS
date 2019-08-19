df5df4b287f4b951bf25fc2043b7fbf6db830079
Orlando Amorim <orlando.filho95@icloud.com>
2019-08-19 01:39:24 -0300

    Make RulePhoneNumber conforms to RuleType

--- Santander/Santander/Library/Eureka Rules/RulePhoneNumber.swift
+++ Santander/Santander/Library/Eureka Rules/RulePhoneNumber.swift
@@ -7,11 +7,33 @@
 
 import Eureka
 
-public class RulePhoneNumber: RuleRegExp {
+public class RulePhoneNumber: RuleType {
     
-    static let phoneNumberRegExpr = "(?:\\+\\d{2}\\s*(?:\\(\\d{2}\\))|(?:\\(\\d{2}\\)))?\\s*(\\d{4,5}\\-?\\d{4})"
+    public var regExpr: String
+    public var id: String?
+    public var validationError: ValidationError
+    public var allowsEmpty = true
     
-    public init(msg: String = "Field value should be a valid phone number!", id: String? = nil) {
-        super.init(regExpr: RulePhoneNumber.phoneNumberRegExpr, allowsEmpty: true, msg: msg, id: id)
+    public init(regExpr: String = "^(\\([0-9]{2}\\))\\s([9]{1})?([0-9]{4})-([0-9]{4})$",
+                allowsEmpty: Bool = true,
+                msg: String = "Field value should be a valid phone number!",
+                id: String? = nil) {
+        self.validationError = ValidationError(msg: msg)
+        self.regExpr = regExpr
+        self.allowsEmpty = allowsEmpty
+        self.id = id
+    }
+    
+    public func isValid(value: String?) -> ValidationError? {
+        if let value = value, !value.isEmpty {
+            let predicate = NSPredicate(format: "SELF MATCHES %@", regExpr)
+            guard predicate.evaluate(with: value) else {
+                return validationError
+            }
+            return nil
+        } else if !allowsEmpty {
+            return validationError
+        }
+        return nil
     }
 }
