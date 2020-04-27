e9def5ea396bf262d4c7e3dbd667dd6f43608e6a
Eduardo <edu.sumiya@gmail.com>
2020-04-27 18:18:15 -0300

    Criação e Estruturação Inicial do Projeto

---
+++ Teste Santander/Teste Santander/Classes/Infrastructure/AppDelegate/AppDelegate.swift
@@ -0,0 +1,60 @@
+//
+//  AppDelegate.swift
+//  Teste Santander
+//
+//  Created by Eduardo Alves Sumiya on 27/04/20.
+//  Copyright © 2020 Eduardo Alves Sumiya. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let controller = DashboardViewController.loadFromNib()
+        let navigationController = UINavigationController(rootViewController: controller)
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window!.rootViewController = navigationController
+        self.window!.makeKeyAndVisible()
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+
+}
+
+extension UIViewController {
+    static func loadFromNib() -> Self {
+        func instantiateFromNib<T: UIViewController>() -> T {
+            return T.init(nibName: String(describing: T.self), bundle: nil)
+        }
+
+        return instantiateFromNib()
+    }
+}
---
+++ Teste Santander/Teste Santander/Classes/Scenes/Dashboard/ViewController/DashboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste Santander
+//
+//  Created by Eduardo Alves Sumiya on 27/04/20.
+//  Copyright © 2020 Eduardo Alves Sumiya. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
